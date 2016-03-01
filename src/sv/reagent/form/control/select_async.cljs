1adc2da455b7f69bfbcf9679bfc08096df812c6f
Maximilian Weber <weber.maximilian@googlemail.com>
2016-03-01 16:02:07 +0100

    Added select-multi-async

--- src/sv/reagent/form/control/select_async.cljs
+++ src/sv/reagent/form/control/select_async.cljs
@@ -6,6 +6,22 @@
 
 (def SelectAsync (r/adapt-react-class js/Select.Async))
 
+(defn- load-options-fn [get-options]
+  (fn [input callback]
+    (go
+      (let [options (<! (get-options input))]
+        (callback
+         nil
+         (clj->js
+          {:options (if (zero? (count input))
+                      (take 100 options)
+                      (filter
+                       #(gstring/caseInsensitiveContains
+                         (:label %)
+                         input)
+                       options))
+           :complete false}))))))
+
 (defn select-one-async
   [{:keys [modifier get-options disabled onChange] :as opts}]
   [SelectAsync
@@ -20,19 +36,24 @@
                  (when onChange
                    (onChange selected)))
      :placeholder (:placeholder (modifier))
-     :loadOptions
-     (fn [input callback]
-       (go
-         (let [options (<! (get-options input))]
-           (callback
-            nil
-            (clj->js
-             {:options (if (zero? (count input))
-                         (take 100 options)
-                         (filter
-                          #(gstring/caseInsensitiveContains
-                            (:label %)
-                            input)
-                          options))
-              :complete false})))))}
+     :loadOptions (load-options-fn get-options)}
+    (dissoc opts :modifier :get-options :onChange))])
+
+(defn select-multi-async
+  [{:keys [modifier get-options disabled onChange] :as opts}]
+  [SelectAsync
+   (merge
+    {:multi true
+     :disabled disabled
+     :value (clj->js (:selected (modifier)))
+     :onChange (fn [selected]
+                 (let [s (js->clj selected :keywordize-keys true)]
+                   (modifier
+                    assoc
+                    :selected (distinct s)
+                    :value (set (map :value s))))
+                 (when onChange
+                   (onChange selected)))
+     :placeholder (:placeholder (modifier))
+     :loadOptions (load-options-fn get-options)}
     (dissoc opts :modifier :get-options :onChange))])
