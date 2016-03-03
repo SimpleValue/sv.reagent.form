c20e63d8ecbd24a7a2a456be44f2b668cd70254d
Maximilian Weber <weber.maximilian@googlemail.com>
2016-03-03 12:30:02 +0100

    integer-input: Added support for negative integers.

--- src/sv/reagent/form/control/integer_input.cljs
+++ src/sv/reagent/form/control/integer_input.cljs
@@ -1,7 +1,8 @@
 (ns sv.reagent.form.control.integer-input
   (:require [sv.reagent.form.control.text-input :as t]
             [goog.string :as gstring]
-            [sv.reagent.form.bind :as b]))
+            [sv.reagent.form.bind :as b]
+            [clojure.string :as str]))
 
 (defn integer-input [modifier]
   [:input
@@ -10,7 +11,8 @@
     {:value (b/get-value modifier str)
      :onChange (fn [e]
                  (let [value (.-value (.-target e))]
-                   (when (gstring/isNumeric value)
+                   (when (gstring/isNumeric
+                          (str/replace value #"^-" ""))
                      ((b/bind-input-value modifier) e))))
      :onBlur (b/bind-value
               modifier
